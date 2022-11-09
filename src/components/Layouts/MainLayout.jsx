77c9459509ff4ae636a720196140ec5eb1e57a92
sayi-victor <109381730+sayi-victor@users.noreply.github.com>
2022-11-09 19:49:02 +0300

    Update MainLayout.jsx

--- src/components/Layouts/MainLayout.jsx
+++ src/components/Layouts/MainLayout.jsx
@@ -1,6 +1,5 @@
 import Header from "../Header";
 import Footer from "../Footer";
-import { Footer } from "../footer";
 import { LeagueTables } from "../LeagueTables";
 
 const MainLayout = ({children}) => {
