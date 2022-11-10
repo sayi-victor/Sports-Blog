ead0f9a1264f33b50559a38d04e5260f3af5e63e
sayi-victor <109381730+sayi-victor@users.noreply.github.com>
2022-11-10 06:12:27 +0300

    Added line 5 to fix the react is not defined error

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+window.React = React // This fixes the react is nt defined error
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
